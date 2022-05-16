caff0bb618d6b79e9a6c2446ede316179aea9847
Peter Jolley <peterjolley456@gmail.com>
2022-05-16 12:13:14 +0930

    Manager test code created

---
+++ Tests/Manager.test.js
@@ -0,0 +1,16 @@
+const Manager = require ("../lib/Manager");
+
+
+describe("Manager", () => {
+    it ("Should return Manager Office number", () => {
+        const testOfficenumber = 82165021;
+        const manager = ("Apricat", 5 ,"Tabby@email.com", testOfficenumber);
+        expect(manager.officeNumber).toBe(testOfficenumber)
+    });
+    it ("Should return Manager form getRole ()", () =>{
+        const testRole = "Manager"
+        const manager = new Manager ("Apricat", 5 ,"Tabby@email.com", 82165021);
+        expect(manager.getRole).toBe(testRole)
+    });
+    it ("")
+})
